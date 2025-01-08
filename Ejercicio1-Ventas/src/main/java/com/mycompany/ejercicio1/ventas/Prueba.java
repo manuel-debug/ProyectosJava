8eeee69944d0ced4bca40ea6accfd8d1815bcbff
manuel-debug <manuelstj73@gmail.com>
2025-01-08 19:59:07 +0100

    Prueba git

---
+++ Ejercicio1-Ventas/src/main/java/com/mycompany/ejercicio1/ventas/Prueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio1.ventas;
+
+/**
+ * Esta clase no hace nada, solo es una prueba para la implementación de GIT
+ * @author Manuel González Vaz
+ */
+public class Prueba {
+    
+}
