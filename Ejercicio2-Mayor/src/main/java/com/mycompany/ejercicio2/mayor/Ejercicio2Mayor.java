c9e83f9a71b57246ce47e490448c36fed0e773ff
manuel-debug <manuelstj73@gmail.com>
2025-01-09 17:51:34 +0100

    Añadido Ejercicio2-Mayor

--- Ejercicio2-Mayor/src/main/java/com/mycompany/ejercicio2/mayor/Ejercicio2Mayor.java
+++ Ejercicio2-Mayor/src/main/java/com/mycompany/ejercicio2/mayor/Ejercicio2Mayor.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.ejercicio2.mayor;
 
 import java.util.Random;
@@ -9,20 +8,21 @@ import java.util.Random;
 
 /**
  * Saber cuál es el mayor de un arreglo.
+ *
  * @author Manuel González Vaz
  */
 public class Ejercicio2Mayor {
 
     public static void main(String[] args) {
-        
+
         /**
          * Array que contendra 5 numeros enteros
          */
-        int[] arrayInt=new int[5];
+        int[] arrayInt = new int[5];
         /**
          * Numero entero el cual es el mayor de arrayInt
          */
-        int mayor=arrayInt[0];
+        int mayor = arrayInt[0];
         /**
          * Variable de tipo scanner que nos servira para introducir los datos
          * por teclado
@@ -32,19 +32,18 @@ public class Ejercicio2Mayor {
          * Variable random me permite generar numeros aleatorios
          */
         Random rand = new Random();
-        
-        
+
         System.out.println("Introduzca los 5 numeros");
         for (int i = 0; i < arrayInt.length; i++) {
             //arrayInt[i]= sc.nextInt();
-            
-            arrayInt[i]=(1+rand.nextInt(5));
+
+            arrayInt[i] = (1 + rand.nextInt(5));
             System.out.println(arrayInt[i]);
-            
-            if(arrayInt[i]>mayor){
-                mayor=arrayInt[i];
+
+            if (arrayInt[i] > mayor) {
+                mayor = arrayInt[i];
             }
         }
-        System.out.println("El numero mayor en el arreglo es "+mayor);
+        System.out.println("El numero mayor en el arreglo es " + mayor);
     }
 }
