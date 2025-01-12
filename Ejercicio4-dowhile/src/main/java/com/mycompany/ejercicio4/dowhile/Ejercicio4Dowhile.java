ab2225e67ca2d9d8538bfd60bc837a77c2b7d520
manuel-debug <manuelstj73@gmail.com>
2025-01-12 20:36:27 +0100

    Añadido Ejercicio4-dowhile

---
+++ Ejercicio4-dowhile/src/main/java/com/mycompany/ejercicio4/dowhile/Ejercicio4Dowhile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4.dowhile;
+
+/**
+ * Programa que pida números, y al poner cero, deje de pedir, y que los sume.
+ * @author Manuel González Vaz
+ */
+public class Ejercicio4Dowhile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
