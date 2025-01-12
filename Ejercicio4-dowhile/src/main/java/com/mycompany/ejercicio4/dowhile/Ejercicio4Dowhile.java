76483b561fa563d8d203373fb4f6da97353d59df
manuel-debug <manuelstj73@gmail.com>
2025-01-12 20:44:14 +0100

    Terminado Ejercicio4-dowhile

--- Ejercicio4-dowhile/src/main/java/com/mycompany/ejercicio4/dowhile/Ejercicio4Dowhile.java
+++ Ejercicio4-dowhile/src/main/java/com/mycompany/ejercicio4/dowhile/Ejercicio4Dowhile.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.ejercicio4.dowhile;
 
+import java.util.Scanner;
+
 /**
  * Programa que pida números, y al poner cero, deje de pedir, y que los sume.
  * @author Manuel González Vaz
@@ -11,6 +13,31 @@ package com.mycompany.ejercicio4.dowhile;
 public class Ejercicio4Dowhile {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /**
+         * Numero que introduce el usuario
+         */
+        int numero=0;
+        /**
+         * Suma total de los numeros que ha introducido el usuario
+         */
+        int total=0;
+        /**
+         * Variable de tipo scanner que nos servira para introducir los datos
+         * por teclado
+         */
+        Scanner sc = new Scanner(System.in);
+        
+        /**
+         * Bucle que no parara de pedir numeros hasta que se introduzca un 0
+         */
+        do{
+            System.out.println("Introduzca numero a sumar\nSi desea parar "
+                    + "introduzca 0");
+            numero=sc.nextInt();
+            total=numero+total;
+        }while(numero!=0);
+        
+        System.out.println("El total de la suma es: "+total);
+        
     }
 }
