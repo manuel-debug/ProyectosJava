4b93d8e7ebb9ac7d0b6a7c34ca73383e8533579c
manuel-debug <manuelstj73@gmail.com>
2025-01-12 21:35:47 +0100

    Terminado Ejercicio6-impares

--- Ejercicio6-impares/src/main/java/com/mycompany/ejercicio6/impares/Ejercicio6Impares.java
+++ Ejercicio6-impares/src/main/java/com/mycompany/ejercicio6/impares/Ejercicio6Impares.java
@@ -1,16 +1,33 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.ejercicio6.impares;
 
 /**
  * Programa que multiplique los 10 primeros números impares.
+ *
  * @author Manuel González Vaz
  */
 public class Ejercicio6Impares {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /**
+         * Numero impar que aumentara hasta el decimo (19)
+         */
+        int numero = 1;
+        /**
+         * Multiplicación total de los numeros impares
+         */
+        long total = 1;
+
+        /**
+         * Creo un bucle for de 10 iteraciones en el cual se mutiplicara los
+         * numeros y se le sumara dos al numero impar para obtener el siguiente
+         */
+        for (int i = 0; i < 10; i++) {
+            total *= numero;
+            numero += 2;
+        }
+        System.out.println("Total: "+total);
     }
 }
