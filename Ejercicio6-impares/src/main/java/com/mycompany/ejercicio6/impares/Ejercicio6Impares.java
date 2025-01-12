8844d4bcdcfb751f49fbe13a2bfa975d3fa1358e
manuel-debug <manuelstj73@gmail.com>
2025-01-12 21:18:22 +0100

    Añadido Ejercicio6-impares

---
+++ Ejercicio6-impares/src/main/java/com/mycompany/ejercicio6/impares/Ejercicio6Impares.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio6.impares;
+
+/**
+ * Programa que multiplique los 10 primeros números impares.
+ * @author Manuel González Vaz
+ */
+public class Ejercicio6Impares {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
