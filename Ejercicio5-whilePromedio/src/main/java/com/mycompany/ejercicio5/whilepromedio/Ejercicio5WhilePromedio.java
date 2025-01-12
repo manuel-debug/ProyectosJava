c1eab1bf3317cd00214a590cece528ab9ca59c07
manuel-debug <manuelstj73@gmail.com>
2025-01-12 20:46:54 +0100

    Añadido Ejercicio5-whilePromedio

---
+++ Ejercicio5-whilePromedio/src/main/java/com/mycompany/ejercicio5/whilepromedio/Ejercicio5WhilePromedio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio5.whilepromedio;
+
+/**
+ * Capturar sólo números positivos, y obtener la media(promedio) de esos números.
+ * @author Manuel González Vaz
+ */
+public class Ejercicio5WhilePromedio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
